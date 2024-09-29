a4683a99c67ff9ee7b6c6e2fa8de29767254b046
Your New Name <mahamdudul408432.com>
2024-09-29 21:17:39 +0600

    set up login logout option user and admin ,

--- resources/views/home/header.blade.php
+++ resources/views/home/header.blade.php
@@ -1,63 +1,77 @@
 <header class="header_section">
-      <nav class="navbar navbar-expand-lg custom_nav-container ">
+    <nav class="navbar navbar-expand-lg custom_nav-container ">
         <a class="navbar-brand" href="index.html">
-          <span>
-            Giftos
-          </span>
+            <span>
+                Giftos
+            </span>
         </a>
-        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-          <span class=""></span>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent"
+            aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+            <span class=""></span>
         </button>
 
         <div class="collapse navbar-collapse" id="navbarSupportedContent">
-          <ul class="navbar-nav  ">
-            <li class="nav-item active">
-              <a class="nav-link" href="index.html">Home <span class="sr-only">(current)</span></a>
-            </li>
-            <li class="nav-item">
-              <a class="nav-link" href="shop.html">
-                Shop
-              </a>
-            </li>
-            <li class="nav-item">
-              <a class="nav-link" href="why.html">
-                Why Us
-              </a>
-            </li>
-            <li class="nav-item">
-              <a class="nav-link" href="testimonial.html">
-                Testimonial
-              </a>
-            </li>
-            <li class="nav-item">
-              <a class="nav-link" href="contact.html">Contact Us</a>
-            </li>
-          </ul>
-          <div class="user_option">
-            <a href=" {{ url('/login') }} ">
-              <i class="fa fa-user" aria-hidden="true"></i>
-              <span>
-                Login
-              </span>
-            </a>
+            <ul class="navbar-nav  ">
+                <li class="nav-item active">
+                    <a class="nav-link" href="index.html">Home <span class="sr-only">(current)</span></a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="shop.html">
+                        Shop
+                    </a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="why.html">
+                        Why Us
+                    </a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="testimonial.html">
+                        Testimonial
+                    </a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="contact.html">Contact Us</a>
+                </li>
+            </ul>
+            <div class="user_option">
 
-            <a href=" {{ url('/register') }} ">
-              <i class="fa fa-vcard" aria-hidden="true"></i>
-              <span>
-                Register
-              </span>
-            </a>
+                @if (Route::has('login'))
+                    @auth
+
+                        <a href="">
+                            <i class="fa fa-shopping-bag" aria-hidden="true"></i>
+                        </a>
 
 
-            <a href="">
-              <i class="fa fa-shopping-bag" aria-hidden="true"></i>
-            </a>
-            <form class="form-inline ">
-              <button class="btn nav_search-btn" type="submit">
-                <i class="fa fa-search" aria-hidden="true"></i>
-              </button>
-            </form>
-          </div>
+                        <form method="POST" action="{{ route('logout') }}">
+                            @csrf
+                            <i class="fa fa-user" aria-hidden="true"></i>
+                            <input class="btn btn-sm" type="submit" value="Logout">
+                        </form>
+                    @else
+                        <a href=" {{ url('/login') }} ">
+                            <i class="fa fa-user" aria-hidden="true"></i>
+                            <span>
+                                Login
+                            </span>
+                        </a>
+
+                        <a href=" {{ url('/register') }} ">
+                            <i class="fa fa-vcard" aria-hidden="true"></i>
+                            <span>
+                                Register
+                            </span>
+                        </a>
+
+
+                    @endauth
+                @endif
+
+
+
+
+            </div>
         </div>
-      </nav>
-    </header>
+    </nav>
+</header>
