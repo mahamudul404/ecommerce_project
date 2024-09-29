c6769c55feb2dda1bf2693cfd4cb37adb9768bd0
Your New Name <mahamdudul408432.com>
2024-09-30 00:37:38 +0600

    cart option add

--- resources/views/home/product_details.blade.php
+++ resources/views/home/product_details.blade.php
@@ -2,41 +2,41 @@
 <html>
 
 <head>
-  <style>
-    .detail-box {
-      padding: 10px 25px;
-    }
-  </style>
+    <style>
+        .detail-box {
+            padding: 10px 25px;
+        }
+    </style>
 
-  @include('home.css')
+    @include('home.css')
 
 </head>
 
 <body>
-  <div class="hero_area">
-    <!-- header section strats -->
-    @include('home.header')
-    <!-- end header section -->
-   
+    <div class="hero_area">
+        <!-- header section strats -->
+        @include('home.header')
+        <!-- end header section -->
 
 
 
-    <section class="shop_section layout_padding">
-    <div class="container">
-        <div class="heading_container heading_center">
-            <h2>
-                Latest Products
-            </h2>
-        </div>
-        <div class="row">
+
+        <section class="shop_section layout_padding">
+            <div class="container">
+                <div class="heading_container heading_center">
+                    <h2>
+                        Latest Products
+                    </h2>
+                </div>
+                <div class="row">
 
 
-           
-              
 
-                <div class="col-sm-6 col-md-10">
-                    <div class="box">
-                        <a href="">
+
+
+                    <div class="col-sm-6 col-md-10">
+                        <div class="box">
+
                             <div class="img-box">
                                 <img src="/product/{{ $product->image }}" alt="">
                             </div>
@@ -48,45 +48,48 @@
                                     Price
                                     <span>
                                         ${{ $product->price }}
-                                  </span>
+                                    </span>
                                 </h6>
                             </div>
 
                             <div class="detail-box">
                                 <h6>
-                                  Category :  {{ $product->category }}
+                                    Category : {{ $product->category }}
                                 </h6>
                                 <h6>
-                                  Available Quantity:
+                                    Available Quantity:
                                     <span>
                                         {{ $product->quantity }}
-                                  </span>
+                                    </span>
                                 </h6>
                             </div>
 
                             <div class="detail-box">
-                              <p>{{$product->description}}</p>
+                                <p>{{ $product->description }}</p>
                             </div>
-                            
+
+
+
+
+                        </div>
                     </div>
+
+
+
+
+
                 </div>
-         
+            </div>
 
-
-
-
-        </div>
-    </div>
-
-</section>
+        </section>
 
 
 
 
 
-   
-@include('home.footer')
- 
+
+        @include('home.footer')
+
 
 </body>
 
--- resources/views/home/products.blade.php
+++ resources/views/home/products.blade.php
@@ -9,11 +9,8 @@
 
 
             @foreach ($products as $product)
-              
-
                 <div class="col-sm-6 col-md-4 col-lg-3">
                     <div class="box">
-                        <a href="">
                             <div class="img-box">
                                 <img src="product/{{ $product->image }}" alt="">
                             </div>
@@ -25,12 +22,19 @@
                                     Price
                                     <span>
                                         ${{ $product->price }}
-                                  </span>
+                                    </span>
                                 </h6>
                             </div>
                             <div style="padding: 10px">
-                              <a class="btn btn-danger" href=" {{url('product_details', $product->id)}} ">Details</a>
+                                <a class="btn btn-danger"
+                                    href=" {{ url('product_details', $product->id) }} ">Details</a>
                             </div>
+
+                            <div>
+                                <a class="btn btn-primary"
+                                    href=" {{ url('add_to_cart', $product->id) }} ">Add to cart</a>
+                            </div>
+
                     </div>
                 </div>
             @endforeach
