9d379efe025621890c856df32a6ea4ad175b14b7
Your New Name <mahamudul408432@gmail.com>
2024-10-02 11:31:26 +0600

    customize table

--- resources/views/home/my_cart.blade.php
+++ resources/views/home/my_cart.blade.php
@@ -18,30 +18,36 @@
         <!-- end slider section -->
     </div>
 
-    <table class=" table table-striped table-bordered table-hover table-sm text-center">
-        <thead class="">
-            <tr class="text-center">
-                <th scope="col">Product Title</th>
-                <th scope="col">Product Price</th>
-                <th scope="col">Product Image</th>
-                <th scope="col">Action</th>
-            </tr>
-        </thead>
+    <div class="container mt-4">
+        <div class="row">
+            <div class="col-md-12">
+                <table class=" table table-striped table-bordered table-hover table-sm text-center">
+                    <thead class="">
+                        <thead class="">
+                            <tr class="text-center">
+                                <th scope="col">Product Title</th>
+                                <th scope="col">Product Price</th>
+                                <th scope="col">Product Image</th>
+                                <th scope="col">Action</th>
+                            </tr>
+                        </thead>
 
-        @foreach ($carts as $cart)
-            <tr class="text-center">
-                <td class="text-center">{{ $cart->product->title }}</td>
-                <td class="text-center">{{ $cart->product->price }}</td>
-                <td class="text-center"><img src="/product/{{ $cart->product->image }}" alt=""
-                        style="width: 100px; height: 100px;"></td>
-                <td>
-                    <a href=" {{ url('remove_cart/'.$cart->id) }} " class="btn btn-danger">Remove</a>
-                </td>
-            </tr>
-        @endforeach
-    </table>
+                        @foreach ($carts as $cart)
+                            <tr class="text-center">
+                                <td class="text-center">{{ $cart->product->title }}</td>
+                                <td class="text-center">{{ $cart->product->price }}</td>
+                                <td class="text-center"><img src="/product/{{ $cart->product->image }}" alt=""
+                                        style="width: 100px; height: 100px;"></td>
+                                <td>
+                                    <a href=" {{ url('remove_cart/' . $cart->id) }} " class="btn btn-danger">Remove</a>
+                                </td>
+                            </tr>
+                        @endforeach
+                </table>
 
-
+            </div>
+        </div>
+    </div>
 
     <!-- end hero area -->
 
